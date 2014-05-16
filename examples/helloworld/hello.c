c06eb8e477681192aac0498548cc5aa27034aaff
Jonathan Anderson <jonathan.anderson@ieee.org>
2014-05-16 16:54:42 -0400

    Add ‘helloworld' example from website.

---
+++ examples/helloworld/hello.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+#include "helloworld.h"
+
+
+int main(int argc, char *argv[])
+{
+	printf("Hello, %s!\n", target());
+	return 0;
+}
---
+++ examples/helloworld/world.c
@@ -0,0 +1,6 @@
+#include "helloworld.h"
+
+const char* target()
+{
+	return "world";
+}
