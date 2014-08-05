85c43e374409e379b514c1f9e0b7f5bebc3758fd
Jonathan Anderson <jonathan.anderson@ieee.org>
2014-08-05 11:40:37 -0230

    Update bootstrap Makefile with latest changes.

--- bootstrap/make/regenerate.sh
+++ bootstrap/make/regenerate.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-fab --format=make -D 'debug=false' ../../fabfile || exit 1
+fab --format=make -D 'debug=false' -D 'asserts=false' ../../fabfile || exit 1
 ./strip-make-paths.sh || exit 1
