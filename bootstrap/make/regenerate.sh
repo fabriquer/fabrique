9c17e270b5110e8f6a11c92ef38fc3a20036b35c
Jonathan Anderson <jonathan.anderson@ieee.org>
2015-02-10 16:41:45 -0330

    Remove regeneration from bootstrap makefile.
    Depending on when various files were created by the revision control
    client, make might first want to re-build the makefile, which requires a
    working Fabrique instance. We don't have one of those yet: that's why
    we're bootstrapping!
    
    Solve the problem by removing the regeneration facility from the bootstrap
    makefile. If we need to regenerate this file, we need to do it explicitly.

--- bootstrap/make/regenerate.sh
+++ bootstrap/make/regenerate.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
 
 fab --format=gmake -D 'debug=false' -D 'asserts=false' ../../fabfile || exit 1
-./strip-make-paths.sh GNUmakefile || exit 1
+awk '/^GNUmakefile : / {exit} {print}' GNUmakefile > tmp || exit 1
+sed -i '' 's/GNUmakefile//' tmp || exit 1
+./strip-make-paths.sh tmp || exit 1
+mv tmp GNUmakefile
