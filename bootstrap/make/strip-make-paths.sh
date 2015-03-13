1b4277466285b579f085d57e3471416f118a6012
Jonathan Anderson <jonathan.anderson@ieee.org>
2015-03-13 15:16:33 -0230

    Allow makefile regeneration to be quiet.
    Typically, when rebuilding the bootstrap Makefile we show a diff of the makefile and the stripped version. If QUIET is set, don't print this diff.

--- bootstrap/make/strip-make-paths.sh
+++ bootstrap/make/strip-make-paths.sh
@@ -14,5 +14,7 @@ sed -i.sedbackup "s#${SRCROOT}#\${srcroot}#g" ${FILENAME} || exit 1
 sed -i.sedbackup "s#^buildroot=	\${buildroot}#buildroot=	.#" ${FILENAME} || exit 1
 sed -i.sedbackup "s#^srcroot=	\${srcroot}#srcroot=	../..#" ${FILENAME} || exit 1
 
-wdiff ${FILENAME}.bak ${FILENAME} | grep '{+' | colordiff
+if [ "${QUIET}" == "" ]; then
+	wdiff ${FILENAME}.bak ${FILENAME} | grep '{+' | colordiff
+fi
 rm ${FILENAME}.bak ${FILENAME}.sedbackup
