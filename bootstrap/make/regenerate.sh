d8e20f02813e5d9f11bc0bed38e43cf6b21c6611
Jonathan Anderson <jonathan.anderson@ieee.org>
2015-03-21 22:38:59 -0230

    Don't include tests in bootstrap makefile.

--- bootstrap/make/regenerate.sh
+++ bootstrap/make/regenerate.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-fab --format=gmake -D 'debug=false' -D 'asserts=false' ../../fabfile || exit 1
+fab --format=gmake -D 'debug=false' -D 'asserts=false' -D 'tests=false' ../../fabfile || exit 1
 awk '/^GNUmakefile : / {exit} {print}' GNUmakefile > tmp || exit 1
 sed -i '' 's/GNUmakefile//' tmp || exit 1
 ./strip-make-paths.sh tmp || exit 1
