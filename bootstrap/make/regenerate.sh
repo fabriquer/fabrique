6b2c167eeaeb1a623d8d45feb28244a05b857f67
Jonathan Anderson <jonathan.anderson@ieee.org>
2015-04-08 17:10:14 -0230

    Un-hardcode byacc path in bootstrap GNUmakefile.

--- bootstrap/make/regenerate.sh
+++ bootstrap/make/regenerate.sh
@@ -3,5 +3,6 @@
 fab --format=gmake -D 'debug=false' -D 'asserts=false' -D 'tests=false' ../../fabfile || exit 1
 awk '/^GNUmakefile : / {exit} {print}' GNUmakefile > tmp || exit 1
 sed -i '' 's/GNUmakefile//' tmp || exit 1
+sed -i '' 's/\/usr\/.*bin\/byacc/byacc/' tmp || exit 1
 ./strip-make-paths.sh tmp || exit 1
 mv tmp GNUmakefile
