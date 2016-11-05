075e1dc034cd76ce20973d290c5fa1542ad1c562
Jonathan Anderson <jonathan.anderson@ieee.org>
2016-11-05 10:25:21 -0700

    Trival: fix search/replace error.
    At some point in the past I did a search and replace for
    Red->Bytestream::Action. I ought to have used \<Red\>, however, as I
    also inadvertently changed "Redistribution" in the license!

--- AST/Function.cc
+++ AST/Function.cc
@@ -7,12 +7,12 @@
  * Cambridge Computer Laboratory under DARPA/AFRL contract (FA8750-10-C-0237)
  * ("CTSRD"), as part of the DARPA CRASH research programme.
  *
- * Bytestream::Actionistribution and use in source and binary forms, with or without
+ * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
  * are met:
- * 1. Bytestream::Actionistributions of source code must retain the above copyright
+ * 1. Redistributions of source code must retain the above copyright
  *    notice, this list of conditions and the following disclaimer.
- * 2. Bytestream::Actionistributions in binary form must reproduce the above copyright
+ * 2. Redistributions in binary form must reproduce the above copyright
  *    notice, this list of conditions and the following disclaimer in the
  *    documentation and/or other materials provided with the distribution.
  *
