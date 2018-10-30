7d931b924c5d826b542d6ad9037a7f2bcc7d470a
Jonathan Anderson <jonathan.anderson@ieee.org>
2018-10-30 16:13:16 -0230

    Add last-chance exception handler.

--- Parsing/ErrorListener.cc
+++ Parsing/ErrorListener.cc
@@ -67,6 +67,10 @@ void ErrorListener::syntaxError(antlr4::Recognizer *r, Token *t, size_t line, si
 	{
 		FAB_ASSERT(false, "unhandled exception of type " + TypeName(e));
 	}
+	catch (...)
+	{
+		FAB_ASSERT(false, "unhandled exception of unknown type");
+	}
 
 	SourceRange src = SourceRange::Span(filename_, line, col + 1, col + 2);
 	throw SyntaxError(message, src, detail);
