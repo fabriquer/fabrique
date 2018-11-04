acc6475f2d71c6ff5a3469f3373a27ca32635bb9
Jonathan Anderson <jonathan.anderson@ieee.org>
2018-11-04 16:28:42 -0330

    Fix AST dumping.
    Actually print the indent string! Also add colours.

--- include/fabrique/ast/ASTDump.hh
+++ include/fabrique/ast/ASTDump.hh
@@ -1,11 +1,12 @@
 /** @file AST/ASTDump.h    Declaration of @ref fabrique::ast::ASTDump. */
 /*
- * Copyright (c) 2013 Jonathan Anderson
+ * Copyright (c) 2013, 2018 Jonathan Anderson
  * All rights reserved.
  *
  * This software was developed by SRI International and the University of
  * Cambridge Computer Laboratory under DARPA/AFRL contract (FA8750-10-C-0237)
- * ("CTSRD"), as part of the DARPA CRASH research programme.
+ * ("CTSRD"), as part of the DARPA CRASH research programme and at Memorial University
+ * of Newfoundland under the NSERC Discovery program (RGPIN-2015-06048).
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -47,10 +48,7 @@ namespace ast {
 class ASTDump : public Visitor
 {
 public:
-	ASTDump(Bytestream& o)
-		: out_(o), indent_(0)
-	{
-	}
+	ASTDump(Bytestream&);
 
 	VISIT(Action)
 	VISIT(Argument)
