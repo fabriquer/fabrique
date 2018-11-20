dab3560f25a51762b20a47350506033092609436
Jonathan Anderson <jonathan.anderson@ieee.org>
2018-11-20 17:50:57 -0330

    Update GNU Make bootstrap path.
    Now we can bootstrap from Make or Ninja again.

--- bootstrap/make/regenerate.sh
+++ bootstrap/make/regenerate.sh
@@ -1,7 +1,13 @@
 #!/bin/sh
 
-fab --format=gmake -D 'debug=false' -D 'asserts=false' -D 'tests=false' ../../fabfile || exit 1
+# Run Fabrique to generate a GNUmakefile here in this directory
+fab --format=gmake ../../fabfile || exit 1
+
+# Remove makefile regeneration: the build host may not have a `fab`
 awk '/^GNUmakefile : / {exit} {print}' GNUmakefile > tmp || exit 1
 sed -i '' 's/GNUmakefile//' tmp || exit 1
+
+# Relativize paths to avoid hardcoding /home/jon/fab/foo
 ./strip-make-paths.sh tmp || exit 1
+
 mv tmp GNUmakefile
