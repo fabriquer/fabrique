f394c844d5fbaaaa8c7f0c00fd4f1b469ab74c30
Jonathan Anderson <jonathan.anderson@ieee.org>
2018-11-20 17:41:48 -0330

    Remove verbose wdiff-based output.
    When regenerating the GNU Make bootstrap path, we don't need to output
    all of the paths that we condense.

--- bootstrap/make/strip-make-paths.sh
+++ bootstrap/make/strip-make-paths.sh
@@ -14,7 +14,4 @@ sed -i.sedbackup "s#${SRCROOT}#\${srcroot}#g" ${FILENAME} || exit 1
 sed -i.sedbackup "s#^buildroot=	\${buildroot}#buildroot=	.#" ${FILENAME} || exit 1
 sed -i.sedbackup "s#^srcroot=	\${srcroot}#srcroot=	../..#" ${FILENAME} || exit 1
 
-if [ "${QUIET}" == "" ]; then
-	wdiff ${FILENAME}.bak ${FILENAME} | grep '{+' | colordiff
-fi
 rm ${FILENAME}.bak ${FILENAME}.sedbackup
