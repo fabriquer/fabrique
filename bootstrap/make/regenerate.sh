9ee286e3e63745a7200727f67f66f1216f98ca63
Jonathan Anderson <jonathan.anderson@mun.ca>
2019-07-14 19:25:09 -0230

    Don't require Clang in the GMake bootstrap either.

--- bootstrap/make/regenerate.sh
+++ bootstrap/make/regenerate.sh
@@ -7,6 +7,10 @@ fab --format=gmake ../../fabfile || exit 1
 awk '/^GNUmakefile : / {exit} {print}' GNUmakefile > tmp || exit 1
 sed -i '' 's/GNUmakefile//' tmp || exit 1
 
+# Use the default host compiler rather than assuming Clang.
+# TODO: remove once the main build description stops making this assumption.
+sed -i '' 's#bin/clang++#bin/c++#' tmp || exit 1
+
 # Relativize paths to avoid hardcoding /home/jon/fab/foo
 ./strip-make-paths.sh tmp || exit 1
 
